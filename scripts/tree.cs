8f54d0f65a7642acd4b91197555e170afe43a4d9
Kyle Young <kyle3208@gmail.com>
2015-10-20 16:21:03 -0500

    Added real tree

--- scripts/tree.cs
+++ scripts/tree.cs
@@ -3,48 +3,43 @@ using System.Collections;
 
 public class tree : MonoBehaviour {
 
-	public Material material;
-	public Mesh     mesh;
-	private int     maxHeight;
-	private float   scale         = 0.7f;
-	private int     currentHeight;
-	private bool    hasTrunk      = false;
+	private GameObject tree_prefab;
+	private enum direction { up, left, right};
+	private int currHeight = 0;
 
+	public tree(GameObject prefab) {
+		tree_prefab = prefab;
+	}
 
-	// Use this for initialization
-	void Start () {
-		maxHeight = 6;
-		gameObject.AddComponent<MeshFilter>().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer>().material = material;
-		if (currentHeight < maxHeight) {
-			GenerateTrunk();
-			if(currentHeight >= 3) {
-				GenerateLeaves();
-			}
+	public void genTree(Vector3 start, int depth, int length, int maxHeight, float theta) {
+		if (depth > 0) {
+			Debug.Log ("Depth = " + depth);
+			Vector3 s = buildPart(start,theta, length);
+			genTree(s, depth - 1, length -1, maxHeight, Random.Range(0, Mathf.PI));
+			genTree(s, depth - 1, length -1, maxHeight, Random.Range(0, Mathf.PI));
+			genTree(s, depth - 1, length -1, maxHeight, Random.Range(0, Mathf.PI));
 		}
-
+	}
+	private Vector3 buildPart(Vector3 start, float theta, int length) {
+		Vector3 end = Vector3.zero;
+		Debug.Log ("Start X = " + start.x);
+		Debug.Log ("Start Y = " + start.y);
+		Instantiate (tree_prefab, start, Quaternion.identity);
+		for (int i = 1; i < length; i++) {
+			end = new Vector3( (findCos(theta) * i) + start.x, (findSin(theta) * i) + start.y, start.z);
+			Instantiate (tree_prefab, end , Quaternion.identity);
+		}
+		Debug.Log ("End X = " + end.x);
+		Debug.Log ("End Y = " + end.y);
+		return end;
 	}
 
-	private void GenerateTrunk() {
-		new GameObject("trunk").AddComponent<tree>().Init(this, Vector3.up);
-	}
+	private float findCos(float theta) {
+		return Mathf.Cos (theta);
+	}	
 
-	private void GenerateLeaves() {
-		new GameObject("leaf").AddComponent<tree>().Init(this, Vector3.left);
-		new GameObject("leaf").AddComponent<tree>().Init(this, Vector3.right);
-		new GameObject("leaf").AddComponent<tree>().Init(this, Vector3.forward);
-		new GameObject("leaf").AddComponent<tree>().Init(this, Vector3.back);
-	}
-	
-	private void Init(tree parent, Vector3 dir) {
-		mesh = parent.mesh;
-		material = parent.material;
-		maxHeight = parent.maxHeight;
-		currentHeight = parent.currentHeight + 1;
-		transform.parent = parent.transform;
-		transform.localPosition = dir * 1;
-		Debug.Log("Depth -> " + currentHeight);
-		Debug.Log("Parent -> " + parent.GetType ());
+	private float findSin(float theta) {
+		return Mathf.Sin (theta);
 	}
 
 }
