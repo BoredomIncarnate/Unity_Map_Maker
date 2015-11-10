ca51fe20f7ac5d5489f90768f26e561c7af9736d
Kyle Young <kyle3208@gmail.com>
2015-11-10 15:26:39 -0600

    Added species to genForest

--- scripts/genForest.cs
+++ scripts/genForest.cs
@@ -16,10 +16,15 @@ public class genForest : ScriptableObject {
 		Debug.Log ("I'm alive");
 		forest f = new forest (radius, density, new point(0, 0, 0));
 		point[] arrForest = f.CreateForest ();
+		species aSpec = new species ();
+		aSpec.generate ();
+		Debug.Log ("Species depth: " + aSpec.depth);
+		Debug.Log ("Species length: " + aSpec.length);
 		foreach (point tree in arrForest) {
 			Debug.Log (string.Format("Tree x: {0}, y: {1}", tree.x, tree.y));
+			aSpec.generate();
 			tree t = new tree(f_prefab);
-			t.genTree(new Vector3(tree.x, 0, tree.y), depth, 5, 20, Mathf.PI / 2);
+			t.genTree(new Vector3(tree.x, 0, tree.y), aSpec.depth, aSpec.length, aSpec.maxHeight, Mathf.PI / 2);
 		}
 	}
 }
