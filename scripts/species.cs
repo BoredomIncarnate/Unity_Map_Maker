83fac58c2715753fb4c093e34843ba904a49ead6
Kyle Young <kyle3208@gmail.com>
2015-11-10 15:26:10 -0600

    Added species generation

---
+++ scripts/species.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class species {
+
+	private int _length, _depth, _maxHeight, _type;
+
+	public species() {
+
+	}
+
+	public void generate() {
+		this._type = (int)Random.Range (0, 2);
+		switch (_type) {
+			case 0:
+				this._length = (int)Random.Range (4, 12);
+				this._depth = (int)Random.Range (1, 8);
+				break;
+			case 1:
+				this._length = (int)Random.Range (1, 5);
+				this._depth = (int)Random.Range(2, 6);
+				break;
+		}
+	}
+
+	public int length {
+		get { return this._length; }
+	}
+
+	public int depth {
+		get { return this._depth; }
+	}
+
+	public int maxHeight {
+		get { return this._maxHeight; }
+	}
+
+
+}
