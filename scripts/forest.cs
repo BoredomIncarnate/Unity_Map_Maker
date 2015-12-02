ae7747061ddd1edb498c7c6f28c68857f853ba41
Kyle Young <kyle3208@gmail.com>
2015-12-02 15:47:11 -0600

    Added texturemap to consturtor. WARNING SUPER ALPHA!

--- scripts/forest.cs
+++ scripts/forest.cs
@@ -7,16 +7,26 @@ public class forest {
 	private double density;
 	private point start;
 	private point[] trees;
+	private Texture2D _map;
 
-	public forest(int s, double d, point startPoint) {
+	public forest(int s, double d, point startPoint, Texture2D map) {
 		start = startPoint;
 		this.r = s;
 		this.density = d;
+		this._map = map;
+
 	}
 
 	public point[] CreateForest() {
 		SetPoints ();
+		//_map.GetPixel (0, 0).r = 255;
 		return trees;
+	}
+
+	public Texture2D map {
+		get { 
+			return _map;
+		}
 	}
 
 	private void SetPoints() {
@@ -25,6 +35,7 @@ public class forest {
 		trees = new point[arrSize];
 		for (int i = 0; i < trees.Length; i++) {
 			trees[i] = CreatePoint(Random.Range(0, 2 * Mathf.PI));
+			_map.GetPixel(trees[i].x, trees[i].y).r = 255;
 		}
 	}
 
